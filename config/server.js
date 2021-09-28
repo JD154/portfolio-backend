3f9eb53e72b785f389cd9352842e527fbe99fe53
Jesus Blanco <jesus.blancof96@gmail.com>
2021-09-28 11:23:21 -0400

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1138b30935b4a97a1743d192748408e3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3908088e-eaf7-4013-8abc-09f7a6012c39'
+};
